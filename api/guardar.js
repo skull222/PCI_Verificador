65e68c954c41663782e1f12540a65d6ab887e90f
skull222 <91346962+skull222@users.noreply.github.com>
2025-07-23 14:25:32 -0600

    Update guardar.js

--- api/guardar.js
+++ api/guardar.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
   const {
     fecha,
     reunion,
+    persona,
     nombre_completo,
     nombre_papa_mama,
     telefono,
@@ -32,6 +33,7 @@ export default async function handler(req, res) {
       INSERT INTO formulario (
         fecha,
         reunion,
+        persona,
         nombre_completo,
         nombre_papa_mama,
         telefono,
@@ -44,12 +46,13 @@ export default async function handler(req, res) {
         verificador,
         red,
         oracion
-      ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14);
+      ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15);
     `;
 
     const values = [
       fecha,
       reunion,
+      persona,
       nombre_completo,
       nombre_papa_mama,
       telefono,
