d46df15f4c2b056e7d61cf0afbc4271618a1b5a0
skull222 <91346962+skull222@users.noreply.github.com>
2025-07-23 13:52:03 -0600

    Update guardar.js

--- api/guardar.js
+++ api/guardar.js
@@ -12,61 +12,49 @@ if (req.method !== "POST") {
 }
 
 const {
-    fecha,
-    reunión,
-    nombre_completo,
-    nombre_papá_mamá,
-    telefono,
-    Colonia,
-    'Alcaldía o Municipio': alcaldia,
-    invitado,
-    celular_de_la_persona_que_invito,
-    como_nos_conocio,
-    celula,
-    verificador,
-    red,
-    oracion
+fecha,
+reunion,
+nombre_completo,
+nombre_papa_mama,
+telefono,
+colonia,
+alcaldia,
+invitado,
+celular_invitado,
+red_conexion,
+celula,
+verificador,
+red,
+oracion
 } = req.body;
 
 try {
-    const query = `
-    INSERT INTO formulario (
-        fecha,
-        reunion,
-        nombre_completo,
-        nombre_papá_mamá,
-        telefono,
-        colonia,
-        alcaldia,
-        invitado,
-        celular_invitado,
-        red_conexion,
-        celula,
-        verificador,
-        red,
-        oracion
-    ) VALUES (
-        $1, $2, $3, $4, $5, $6, $7,
-        $8, $9, $10, $11, $12, $13, $14
-    )
-    `;
+const query = `
+INSERT INTO formulario (
+fecha, reunion, nombre_completo, nombre_papa_mama,
+telefono, colonia, alcaldia, invitado, celular_invitado,
+red_conexion, celula, verificador, red, oracion
+) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+`;
 
-    await pool.query(query, [
-    fecha,
-    reunión,
-    nombre_completo,
-    nombre_papá_mamá,
-    telefono,
-    Colonia,
-    alcaldia,
-    invitado,
-    celular_de_la_persona_que_invito,
-    como_nos_conocio,
-    celula,
-    verificador,
-    red,
-    oracion
-    ]);
+
+await pool.query(query, [
+fecha,
+reunion,
+nombre_completo,
+nombre_papa_mama,
+telefono,
+colonia,
+alcaldia,
+invitado,
+celular_invitado,
+red_conexion,
+celula,
+verificador,
+red,
+oracion
+]);
+
 
     res.status(200).json({ mensaje: "Formulario guardado correctamente" });
 } catch (error) {
