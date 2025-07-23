d0f2447be57f1d8c52fa88804d357db706fb4098
skull222 <91346962+skull222@users.noreply.github.com>
2025-07-23 12:13:58 -0600

    Update guardar.js

--- api/guardar.js
+++ api/guardar.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-connectionString: process.env.DATABASE,
+connectionString: process.env.DATABASE_URL,
 ssl: { rejectUnauthorized: false },
 });
 
